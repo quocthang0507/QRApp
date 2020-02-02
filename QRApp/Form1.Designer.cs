29ce7152d707fb0696cab9ff9b5764fc9502eaac
La Quốc Thắng <quocthang0507@gmail.com>
2020-02-02 22:36:23 +0700

    Add function to project

--- QRApp/Form1.Designer.cs
+++ QRApp/Form1.Designer.cs
@@ -30,15 +30,27 @@
 		{
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.groupBox2 = new System.Windows.Forms.GroupBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.tbxContent = new System.Windows.Forms.TextBox();
-			this.btnGenerate = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.pbxPreview = new System.Windows.Forms.PictureBox();
 			this.label2 = new System.Windows.Forms.Label();
-			this.pbx_Preview = new System.Windows.Forms.PictureBox();
+			this.btnGenerate = new System.Windows.Forms.Button();
+			this.tbxContent_First = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.tbxContent_After = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.btnDecode = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.pbxImage = new System.Windows.Forms.PictureBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.savePicture = new System.Windows.Forms.SaveFileDialog();
+			this.openPicture = new System.Windows.Forms.OpenFileDialog();
+			this.btnTransfer = new System.Windows.Forms.Button();
 			this.tableLayoutPanel1.SuspendLayout();
 			this.groupBox1.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pbx_Preview)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbxPreview)).BeginInit();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbxImage)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
@@ -60,10 +72,12 @@
 			// 
 			// groupBox1
 			// 
-			this.groupBox1.Controls.Add(this.pbx_Preview);
+			this.groupBox1.Controls.Add(this.btnTransfer);
+			this.groupBox1.Controls.Add(this.btnSave);
+			this.groupBox1.Controls.Add(this.pbxPreview);
 			this.groupBox1.Controls.Add(this.label2);
 			this.groupBox1.Controls.Add(this.btnGenerate);
-			this.groupBox1.Controls.Add(this.tbxContent);
+			this.groupBox1.Controls.Add(this.tbxContent_First);
 			this.groupBox1.Controls.Add(this.label1);
 			this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.groupBox1.Location = new System.Drawing.Point(13, 13);
@@ -73,42 +87,25 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "QR Code Generator";
 			// 
-			// groupBox2
+			// btnSave
 			// 
-			this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.groupBox2.Location = new System.Drawing.Point(469, 13);
-			this.groupBox2.Name = "groupBox2";
-			this.groupBox2.Size = new System.Drawing.Size(451, 493);
-			this.groupBox2.TabIndex = 1;
-			this.groupBox2.TabStop = false;
-			this.groupBox2.Text = "QR Code Reader";
+			this.btnSave.Location = new System.Drawing.Point(188, 145);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 3;
+			this.btnSave.Text = "Save as...";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
 			// 
-			// label1
+			// pbxPreview
 			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(6, 39);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(56, 16);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Content:";
-			// 
-			// tbxContent
-			// 
-			this.tbxContent.Location = new System.Drawing.Point(83, 39);
-			this.tbxContent.Multiline = true;
-			this.tbxContent.Name = "tbxContent";
-			this.tbxContent.Size = new System.Drawing.Size(300, 100);
-			this.tbxContent.TabIndex = 1;
-			// 
-			// btnGenerate
-			// 
-			this.btnGenerate.Location = new System.Drawing.Point(180, 147);
-			this.btnGenerate.Name = "btnGenerate";
-			this.btnGenerate.Size = new System.Drawing.Size(85, 25);
-			this.btnGenerate.TabIndex = 2;
-			this.btnGenerate.Text = "Generate";
-			this.btnGenerate.UseVisualStyleBackColor = true;
-			this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+			this.pbxPreview.BackColor = System.Drawing.Color.White;
+			this.pbxPreview.Location = new System.Drawing.Point(83, 178);
+			this.pbxPreview.Name = "pbxPreview";
+			this.pbxPreview.Size = new System.Drawing.Size(300, 300);
+			this.pbxPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbxPreview.TabIndex = 4;
+			this.pbxPreview.TabStop = false;
 			// 
 			// label2
 			// 
@@ -119,15 +116,130 @@
 			this.label2.TabIndex = 3;
 			this.label2.Text = "Preview:";
 			// 
-			// pbx_Preview
+			// btnGenerate
 			// 
-			this.pbx_Preview.BackColor = System.Drawing.Color.White;
-			this.pbx_Preview.Location = new System.Drawing.Point(83, 178);
-			this.pbx_Preview.Name = "pbx_Preview";
-			this.pbx_Preview.Size = new System.Drawing.Size(300, 300);
-			this.pbx_Preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-			this.pbx_Preview.TabIndex = 4;
-			this.pbx_Preview.TabStop = false;
+			this.btnGenerate.Location = new System.Drawing.Point(107, 145);
+			this.btnGenerate.Name = "btnGenerate";
+			this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+			this.btnGenerate.TabIndex = 2;
+			this.btnGenerate.Text = "Generate";
+			this.btnGenerate.UseVisualStyleBackColor = true;
+			this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+			// 
+			// tbxContent_First
+			// 
+			this.tbxContent_First.Location = new System.Drawing.Point(83, 39);
+			this.tbxContent_First.Multiline = true;
+			this.tbxContent_First.Name = "tbxContent_First";
+			this.tbxContent_First.Size = new System.Drawing.Size(300, 100);
+			this.tbxContent_First.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(56, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Content:";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.tbxContent_After);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.btnDecode);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.pbxImage);
+			this.groupBox2.Controls.Add(this.btnBrowse);
+			this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.groupBox2.Location = new System.Drawing.Point(469, 13);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(451, 493);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "QR Code Reader";
+			// 
+			// tbxContent_After
+			// 
+			this.tbxContent_After.Location = new System.Drawing.Point(85, 363);
+			this.tbxContent_After.Multiline = true;
+			this.tbxContent_After.Name = "tbxContent_After";
+			this.tbxContent_After.ReadOnly = true;
+			this.tbxContent_After.Size = new System.Drawing.Size(300, 100);
+			this.tbxContent_After.TabIndex = 8;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(8, 363);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(56, 16);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Content:";
+			// 
+			// btnDecode
+			// 
+			this.btnDecode.Location = new System.Drawing.Point(220, 28);
+			this.btnDecode.Name = "btnDecode";
+			this.btnDecode.Size = new System.Drawing.Size(75, 23);
+			this.btnDecode.TabIndex = 6;
+			this.btnDecode.Text = "Decode";
+			this.btnDecode.UseVisualStyleBackColor = true;
+			this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(8, 57);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(49, 16);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Image:";
+			// 
+			// pbxImage
+			// 
+			this.pbxImage.BackColor = System.Drawing.Color.White;
+			this.pbxImage.Location = new System.Drawing.Point(85, 57);
+			this.pbxImage.Name = "pbxImage";
+			this.pbxImage.Size = new System.Drawing.Size(300, 300);
+			this.pbxImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pbxImage.TabIndex = 5;
+			this.pbxImage.TabStop = false;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Location = new System.Drawing.Point(139, 28);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowse.TabIndex = 5;
+			this.btnBrowse.Text = "Browse...";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+			// 
+			// savePicture
+			// 
+			this.savePicture.FileName = "qrImage";
+			this.savePicture.Filter = "JPEG Files|*.jpg|PNG Files|*.png|BMP Files|*.bmp";
+			this.savePicture.FilterIndex = 2;
+			this.savePicture.RestoreDirectory = true;
+			this.savePicture.Title = "Save this QR code";
+			// 
+			// openPicture
+			// 
+			this.openPicture.Filter = "JPEG Files|*.jpg|PNG Files|*.png|BMP Files|*.bmp";
+			this.openPicture.FilterIndex = 2;
+			this.openPicture.RestoreDirectory = true;
+			this.openPicture.Title = "Select a QR Code";
+			// 
+			// btnTransfer
+			// 
+			this.btnTransfer.Location = new System.Drawing.Point(269, 145);
+			this.btnTransfer.Name = "btnTransfer";
+			this.btnTransfer.Size = new System.Drawing.Size(75, 23);
+			this.btnTransfer.TabIndex = 4;
+			this.btnTransfer.Text = ">>";
+			this.btnTransfer.UseVisualStyleBackColor = true;
+			this.btnTransfer.Click += new System.EventHandler(this.btnTransfer_Click);
 			// 
 			// Form1
 			// 
@@ -141,7 +253,10 @@
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pbx_Preview)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pbxPreview)).EndInit();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbxImage)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -151,11 +266,21 @@
 		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.GroupBox groupBox2;
-		private System.Windows.Forms.TextBox tbxContent;
+		private System.Windows.Forms.TextBox tbxContent_First;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button btnGenerate;
-		private System.Windows.Forms.PictureBox pbx_Preview;
+		private System.Windows.Forms.PictureBox pbxPreview;
 		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.SaveFileDialog savePicture;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.OpenFileDialog openPicture;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.PictureBox pbxImage;
+		private System.Windows.Forms.Button btnDecode;
+		private System.Windows.Forms.TextBox tbxContent_After;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Button btnTransfer;
 	}
 }
 
--- QRApp/Form1.cs
+++ QRApp/Form1.cs
@@ -1,6 +1,8 @@
-﻿using QRCoder;
+﻿using Bytescout.BarCodeReader;
+using QRCoder;
 using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -8,8 +10,6 @@ namespace QRApp
 {
 	public partial class Form1 : Form
 	{
-		QRCodeGenerator qrGenerator;
-
 		public Form1()
 		{
 			InitializeComponent();
@@ -17,14 +17,67 @@ namespace QRApp
 
 		private void btnGenerate_Click(object sender, EventArgs e)
 		{
-			new Thread(() =>
-		   {
-			   qrGenerator = new QRCodeGenerator();
-			   QRCodeData qrCodeData = qrGenerator.CreateQrCode(tbxContent.Text, QRCodeGenerator.ECCLevel.Q);
-			   QRCode qrCode = new QRCode(qrCodeData);
-			   Bitmap qrCodeImage = qrCode.GetGraphic(20);
-			   pbx_Preview.Image = qrCodeImage;
-		   }).Start();
+			QRCodeGenerator qrGenerator = new QRCodeGenerator();
+			QRCodeData qrCodeData = qrGenerator.CreateQrCode(tbxContent_First.Text, QRCodeGenerator.ECCLevel.Q);
+			QRCode qrCode = new QRCode(qrCodeData);
+			Bitmap qrCodeImage = qrCode.GetGraphic(20);
+			pbxPreview.Image = qrCodeImage;
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			if (pbxPreview.Image != null)
+			{
+				DialogResult result = savePicture.ShowDialog();
+				if (result == DialogResult.OK)
+				{
+					string path = savePicture.FileName;
+					switch (savePicture.FilterIndex)
+					{
+						case 1:
+							pbxPreview.Image.Save(path, ImageFormat.Jpeg);
+							break;
+						case 2:
+							pbxPreview.Image.Save(path, ImageFormat.Png);
+							break;
+						case 3:
+							pbxPreview.Image.Save(path, ImageFormat.Bmp);
+							break;
+						default:
+							pbxPreview.Image.Save(path);
+							break;
+					}
+				}
+			}
+		}
+
+		private void btnBrowse_Click(object sender, EventArgs e)
+		{
+			DialogResult result = openPicture.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				string path = openPicture.FileName;
+				pbxImage.ImageLocation = path;
+			}
+		}
+
+		private void btnDecode_Click(object sender, EventArgs e)
+		{
+			if (pbxImage.Image != null)
+			{
+				Reader reader = new Reader();
+				reader.RegistrationName = "demo";
+				reader.RegistrationKey = "demo";
+				reader.BarcodeTypesToFind.QRCode = true;
+				FoundBarcode[] barcodes = reader.ReadFrom((Bitmap)pbxImage.Image);
+				tbxContent_After.Text = barcodes[0].Value;
+			}
+		}
+
+		private void btnTransfer_Click(object sender, EventArgs e)
+		{
+			if (pbxPreview.Image != null)
+				pbxImage.Image = pbxPreview.Image;
 		}
 	}
 }
